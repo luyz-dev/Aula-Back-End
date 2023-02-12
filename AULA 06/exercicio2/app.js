3dcb4d2fc9f3aebcb68aa8ed2d3c62ce1a45c39d
LuizGustavo <luizgustavo.sp2020@gmail.com>
2023-02-12 15:28:53 -0300

    Exercicio 02 Finalizado

---
+++ AULA 06/exercicio2/app.js
@@ -0,0 +1,34 @@
+/************************************************************************************************
+ * Objetivo: Gerenciador de Cálculos de Tabuada
+ * Autor: Luiz Gustavo
+ * Data: 12/02/2023
+ * Versão: 1.0
+************************************************************************************************/
+
+var readline = require('readline')
+const calcTabuadas = require('./modulo/calcTabuadas.js')
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+console.log('************* Gerenciador de Tabuadas *****************')
+entradaDados.question('Digite a tabuada inicial: \n', function (tabInicial) {
+    let tabuadaInicial = tabInicial
+
+    entradaDados.question('Digite a tabuada final: \n', function (tabFinal) {
+        let tabuadaFinal = tabFinal
+
+        entradaDados.question('Digite o minimo multiplicador: \n', function (minMultiplicador) {
+            let multiplicadorMinimo = minMultiplicador
+
+            entradaDados.question('Digite o maximo multiplicador: \n', function (maxMultiplicador) {
+                let multiplicadorMaximo = maxMultiplicador
+                let calculoTabuadas = Number()
+                calculoTabuadas = calcTabuadas.tabuadas(tabuadaInicial, tabuadaFinal, multiplicadorMinimo, multiplicadorMaximo)
+                console.log('************* Fim Das Tabuadas *****************')
+            })
+        })
+    })
+})
---
+++ AULA 06/exercicio2/modulo/calcTabuadas.js
@@ -0,0 +1,44 @@
+/************************************************************************************************
+ * Objetivo: Tabuadas
+ * Autor: Luiz Gustavo
+ * Data: 12/02/2023
+ * Versão: 1.0
+************************************************************************************************/
+
+const tabuadas = function (tabuadaInicial, tabuadaFinal, multiplicadorMinimo, multiplicadorMaximo) {
+    let inicialTabuada = Number(String(tabuadaInicial).replace(',','.'))
+    let finalTabuada = Number(String(tabuadaFinal).replace(',','.'))
+    let minMultiplicador = Number(String(multiplicadorMinimo).replace(',','.'))
+    let maxMultiplicador = Number(String(multiplicadorMaximo).replace(',','.'))
+
+    if (maxMultiplicador == '' || minMultiplicador == '' || inicialTabuada == '' || finalTabuada == '') {
+        console.log('ERRO: Digite todos os valores')
+    } else if (maxMultiplicador < 2 || maxMultiplicador > 100 || minMultiplicador < 2 || minMultiplicador > 100) {
+        console.log('ERRO: Digite um número válido, Entre 2 e 100')
+    } else if (maxMultiplicador <= minMultiplicador) {
+        console.log('ERRO: Maximo multiplicador tem que ser maior que o minimo multiplicador')
+    } else if (finalTabuada <= inicialTabuada) {
+        console.log('ERRO: Tabuada final tem que ser maior que a inicial')
+    } else if (tabuadaInicial < 1 || tabuadaFinal > 50) {
+        console.log('ERRO: A tabuada tem que começar no minimo 1 e a final só pode ir até 50')
+    } else {
+        let resultado
+        let minMultiplicadorCalculado
+
+        while (inicialTabuada <= finalTabuada) {
+            console.log('Tabuda do: ' + inicialTabuada)
+            minMultiplicadorCalculado = Number(minMultiplicador)
+            while (minMultiplicadorCalculado <= maxMultiplicador) {
+                resultado = inicialTabuada * minMultiplicadorCalculado
+                console.log(`${inicialTabuada} x ${minMultiplicadorCalculado} = ${resultado}`)
+                minMultiplicadorCalculado++
+            }
+            inicialTabuada++
+            console.log('******************************')
+        } 
+    }
+}
+
+module.exports = {
+    tabuadas
+}
